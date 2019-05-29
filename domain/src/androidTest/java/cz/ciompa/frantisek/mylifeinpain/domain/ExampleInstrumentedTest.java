a81f9be1d421fa93aabe850129c9040ca7eafcf3
František Ciompa <frantisek.ciompa@gmail.com>
2019-05-29 13:48:41 +0200

    refactoring to MyLifeInPain

--- domain/src/androidTest/java/cz/ciompa/frantisek/mylifeinpain/domain/ExampleInstrumentedTest.java
+++ domain/src/androidTest/java/cz/ciompa/frantisek/mylifeinpain/domain/ExampleInstrumentedTest.java
@@ -20,6 +20,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cz.ictsystem.mypaindiary.domain.test", appContext.getPackageName());
+        assertEquals("cz.ciompa.frantisek.mylifeinpain.domain.test", appContext.getPackageName());
     }
 }
